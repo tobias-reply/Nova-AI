d76f0d9e5c4f1f22313e90c0ecc346d52f4dbfca
Tobias Kapp <t.kapp@reply.de>
2025-04-09 08:37:49 +0200

    Followed documentation

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,20 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data, MODEL_ID, generateHaikuFunction } from "./data/resource";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export const backend = defineBackend({
   auth,
   data,
+  generateHaikuFunction,
 });
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ["bedrock:InvokeModel"],
+    resources: [
+      `arn:aws:bedrock:*::foundation-model/${MODEL_ID}`,
+    ],
+  })
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,50 @@
-"use client";
-import { Amplify } from "aws-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-import outputs from "@/amplify_outputs.json";
-import Chat from './components/Chat';
+import type { Schema } from '@/amplify/data/resource';
+import type { FormEvent } from 'react';
+import { useState } from 'react';
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import outputs from '@/amplify_outputs.json';
 
-// Initialize Amplify configuration
 Amplify.configure(outputs);
 
-function App() {
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [prompt, setPrompt] = useState<string>('');
+  const [answer, setAnswer] = useState<string | null>(null);
+
+  const sendPrompt = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { data, errors } = await client.queries.generateHaiku({
+      prompt
+    });
+
+    if (!errors) {
+      setAnswer(data);
+      setPrompt('');
+    } else {
+      console.log(errors);
+    }
+  };
+
   return (
-    <main className="container">
-      <h1>Amazon Bedrock Chat</h1>
-      <div className="chat-wrapper">
-        <Chat />
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 dark:text-white">
+      <div>
+        <h1 className="text-3xl font-bold text-center mb-4">Haiku Generator</h1>
+        <form className="mb-4 self-center max-w-[500px]" onSubmit={sendPrompt}>
+          <input
+            className="text-black p-2 w-full"
+            placeholder="Enter a prompt..."
+            name="prompt"
+            value={prompt}
+            onChange={(event) => setPrompt(event.target.value)}
+          />
+        </form>
+        <div className="text-center">
+          <pre>{answer}</pre>
+        </div>
       </div>
     </main>
   );
 }
-
-// Wrap with authenticator if you need authentication
-export default App;
