a1b48d46cbcbe27db0b57c089b5c34ca1e340e14
Tobias Kapp <t.kapp@reply.de>
2025-04-09 09:27:24 +0200

    Nova Lite

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -5,7 +5,7 @@ import {
   defineFunction,
 } from "@aws-amplify/backend";
 
-export const MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0";
+export const MODEL_ID = "amazon.nova-lite-v1:0";
 
 export const generateHaikuFunction = defineFunction({
   entry: "./generateHaiku.ts",
